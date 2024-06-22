7bbe8a64612b702be36331943cb55b621119b25b
Melissa Perez <150316476+aeceey@users.noreply.github.com>
2024-06-22 16:34:17 +0800

    Create Reservation.java

---
+++ Reservation.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Reservation {
+    private String guestName;
+    private LocalDate inDate;
+    private LocalDate outDate;
+    private String roomInfoLink;
+
+    private double totalPrice;
+    private double breakdownCost; // not sure about the data type yet
+
+
+}
