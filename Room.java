9006c252cd33c409d957808808b2aa4aaf8e0663
Kylie Fresto <kylie_fresto@dlsu.edu.ph>
2024-06-22 23:17:31 +0800

    Added toString method

--- Room.java
+++ Room.java
@@ -1,28 +1,32 @@
 public class Room {
-    private String name;  // should be unique? 
+    private String name; // Should be unique within a hotel
     private double price;
 
-    public Room(String name, double price){
+    public Room(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-        public void setName(String name){
-            this.name = name;
-        }
 
-    public double getPrice(){
-       return price;
-    }   
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setPrice(double price){
-            this.price = price;
-        }
+    public double getPrice() {
+        return price;
+    }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-    
-
+    public String toString() {
+        return "Room{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
