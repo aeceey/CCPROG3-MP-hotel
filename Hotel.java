ba71df6591801110813596670fd9c6b90cb701ab
Melissa Perez <150316476+aeceey@users.noreply.github.com>
2024-06-22 16:31:32 +0800

    Create Hotel.java

---
+++ Hotel.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Hotel {
+    private String name;
+    private ArrayList<Room> rooms;
+    private ArrayList<Reservation> reservations;
+    private double basePrice;
+
+    public Hotel(String name, int roomCount) {
+        this.name = name;
+
+        this.rooms = new ArrayList<>();
+        
+        this.reservations = new ArrayList<>();
+        this.basePrice = 1299.0;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+}
