43d6d5a1b81958b5de58dcec17e34fdfb0c64e2e
Melissa Perez <melissa_perez@dlsu.edu.ph>
2024-07-31 10:20:47 +0800

    Javadoc for HRSApplication added

--- HRSApplication.java
+++ HRSApplication.java
@@ -1,8 +1,19 @@
+/**
+ * The HRSApplication class is where the Hotel Reservation System will start.
+ * This class is the one who initiates the model, view, and controller of the system.
+ */
 public class HRSApplication {
+    /**
+     * The main method that will initialize the HRS program. It creates the MV and
+     * sets it to be visible.
+     * 
+     * @param args - default parameter
+     */    
     public static void main(String[] args) {
         HRS model = new HRS();
         MainView view = new MainView();
         new HRSController(model, view);
         view.setVisible(true);
+        
     }
 }
